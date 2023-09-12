6e6da3f0ed22897ddd5bd58be624b6d7afbdf5f0
marisimoesdev <143559652+marisimoesdev@users.noreply.github.com>
2023-09-12 13:46:10 -0300

    Área do administrador
    Inseri a página do administrador, onde pode criar a conta, entrar no cartório e deletar a conta.

--- Cartorio_EBAC.c
+++ Cartorio_EBAC.c
@@ -3,7 +3,7 @@
 #include <locale.h> //Biblioteca de alocações de texto por região
 #include <string.h> //Biblioteca responsável pelas strings
 
-int cadastrar() //Função para cadastro do usuário
+int cadastrar_usuario() //Função para cadastro do usuário
 {
 	setlocale(LC_ALL, "portuguese"); //Definindo o idioma
 	
@@ -63,7 +63,7 @@ int cadastrar() //Função para cadastro do usuário
 	system("pause"); //Pausando a página. Assim o usuário sós sairá dela quando quiser
 }
 
-int consultar() //Função para a consulta do usuário
+int consultar_usuario() //Função para a consulta do usuário
 {
 	setlocale(LC_ALL, "portuguese"); //Definindo o idioma
 	
@@ -91,7 +91,7 @@ int consultar() //Função para a consulta do usuário
 	system("pause"); //Pausando a tela, para que o usuário volte para o menu quando desejar
 }
 
-int deletar() //Função para deletar usuário
+int deletar_usuario() //Função para deletar usuário
 {
 	char cpf[40]; //Criando novamente a variáel cpf char/string, pois estamos em uma função diferente, essa variável/string é para a alocação do cpf que será digitado pelo o usuário
 	int escolha=0; //Variável para a alocação da escolha do usuário, no momento da escolha
@@ -112,7 +112,7 @@ int deletar() //Função para deletar usuário
 		printf("\nTem certeza que deseja deletar o CPF "); //Coletando informações do usuário
 		printf("%s", cpf); //Esse printf em questão não irá mostrar o “%s”, mas sim o o que está armazenada na variável char/string
 		printf(" do nosso sistema?\n"); //Coletando informações do usuário
-		printf("\nDigite “1” para DELETAR o CPF e “2” para VOLTAR ao menu: "); //Coletando informações do usuário
+		printf("\nDigite “1” para DELETAR o CPF e “2” para VOLTAR ao menu do cartório: "); //Coletando informações do usuário
 		scanf("%d", &escolha); //Escaneando e armazenando a resposta do usuário, no caso essa será alocada na variável tipo=int/nome=escolha. Por estar escaneando e armazenando a escolha do usuário não é obrigatório colocar o system(“pause”);
 		fclose(file); //Fechando o arquivo
 	
@@ -139,11 +139,95 @@ int deletar() //Função para deletar usuário
 	//Final da validação
 }
 
-int main()
+int criar_conta()
+{
+	char nome_conta[100]; //Variável string/char, que levará que será o nome do arquivo
+	char celular_telefone[50]; //Variável string/char, que receberá o valor do telefone do usuário
+	char senha[50]; //Variável string/char, que receberá o valor da senha do usuário
+	
+	printf("Insira seus dados abaixo para criar a sua conta.\n\n"); //Mensagem para o usuário
+	printf("Telefone/celular: "); //Coletando informações do usuário
+	scanf("%s", celular_telefone); //Escaneando e alocando a informação do usuário na string/char “celular_telefone”
+	
+	strcpy(nome_conta, celular_telefone); //Copiando o valor da string “celular_telefone” para a string “nome_conta”
+	
+	FILE *arq; //Criando o arquivo, você está falando para ele procurar nas nossas bibliotecas alguma estrutura chamada FILE, porque a gente vai criar um arquivo.
+	arq = fopen(nome_conta, "w"); //Abrindo o arquivo chamado “nome_conta”. A letra “w” vêm de write, ou seja, é para escrever
+	fprintf(arq, celular_telefone); //Esse comando você está falando para ele salvar o valor da string “celular_telefone” dentro do arquivo
+	fclose(arq); //Fechando o arquivo
+	
+	printf("Senha: "); //Coletando informações do usuário
+	scanf("%s", senha); //Escaneando e alocando a informação na string “senha”
+	
+	arq = fopen(nome_conta, "a"); //Abrindo o arquivo “nome_conta”, e a letra “a” para atulizar um arquivo já existente
+	fprintf(arq, senha); //Pedindo para salvar o valor da string “senha” dentro do arquivo
+	fclose(arq); //Fechando o arquivo
+	
+	system("pause"); //Pausando a tela para que assim o usuário consiga preencher os dados
+}
+
+int entrar_cartorio()
+{
+	char celular_telefone[50]="a"; //Chamando a variável que já foi criada anteriormente. Esse valor dessa variável será sobreposto ao que o usuário digitar.. então não faz diferença. Acho que por ser uma string tem que estar entre aspas o valor
+	char senha[50]="a"; //Chamando a variável que já foi criada anteriormente. Esse valor dessa variável será sobreposto ao que o usuário digitar.. então não faz diferença. Acho que por ser uma string tem que estar entre aspas o valor
+	char informacao_usuario[200]; //Essa variável irá receber as informações do usuário
+	
+	printf("Insira os seus dados abaixo para entrar no cartório EBAC.\n\n"); //Mensagem para o usuário
+	printf("Telefone/celular: "); //Coletando informações do usuário
+	scanf("%s", celular_telefone); //Escaneando e armazenando a informação do usuário na string “telefone_celular”
+	
+	printf("Senha: "); //Coletando informações do usuário
+	scanf("%s", senha); //Escaneando e alocando a informação do usuário na string “senha”
+		
+	FILE *arq; //Com esse comando você está pedindo pra ele acessar na biblioteca e procurar uma estrutura de arquivo
+	arq = fopen(celular_telefone, "r"); //Esse comando é pra ele abrir o arquivo “celualr_telefone” e a letra “r” é pra ele leia o que está dentro do arquivo
+	while (fgets(informacao_usuario, 200, arq) != NULL) //Caso ele ache um arquivo com tenha o nome que bata com a informação dada pelo o usuário, que nessa situação seria a variável “celular_telefone”, já que a mesma que dá o nome ao arquivo. Nesse comando você está falando pra ele alocar o conteúdo do arquivo na variável “informacao_usuario”, ele vai gravar todos os caracteres na variável até que seja NULO, ou seja até acabar os caracteres dentro desse arquivo
+	fclose(arq); //Fechando o arquivo
+	
+	strcat (celular_telefone, senha); //Esse comando faz a concatenação das strings, ou seja, ele liga o valor da 1º string com a 2º string
+	
+	if (strcmp(informacao_usuario, celular_telefone)==0) //Esse comando é para comparar o valor alocado nas strings, é “==0” porque se as informações nas strings forem iguais o esultado é 0
+	{
+		cartorio(); //Chamando a função “cartorio”, ou seja, se a comparação das strings der 0, ele terá acesso ao cartório da EBAC
+	}
+	else //Se caso as informações dada pelo o usuário não baterem na hora da comparação
+	{
+		system("cls"); //Limpando a tela
+		printf("Telefone/celular ou senha incorretos.\n\n"); //Mensagem para o usuário
+	}
+	system("pause"); //Pausando a tela
+}
+
+int deletar_conta()
+{
+	char nome_conta[100]; //Chamando a variável que já foi criada anteriormente
+	
+	printf("Digite o telefone/celular da conta que você deseja deletar: "); //Coletando informações do usuário
+	scanf("%s", nome_conta); //Escanaando e alocando a informação do usuário na string “nome_conta”
+	
+	FILE *arq; //Com esse comando você está pedindo pra ele acessar na biblioteca e procurar uma estrutura de arquivo
+	arq = fopen(nome_conta, "r"); //Abrindo o arquivo correspondente com a informação dada pelo usuário. A letra “r” é para ler
+	if (arq == NULL) //Se caso a informação dada pelo o usuário não exista
+	{
+		printf("\nEssa conta não existe.\n\n"); //Mensagem para o usuário
+	}
+	fclose(arq); //Fechando o arquivo
+	if (arq != NULL) //Caso o informação do usuário exista
+	{
+		system("cls"); //Limpando a tela
+		remove(nome_conta); //Chamando o comando “remove” nas bibliotecas, para que assim seja deletado a conta desejado
+		arq = fopen(nome_conta, "r"); //Abrindo o arquivo correspondente com a informação dada pelo usuário. A letra “r” é para ler
+		printf("Conta deletada com sucesso.\n\n"); //Mensagem para o usuário
+		fclose(arq); //Fechando o arquivo
+	}
+	system("pause"); //Pausando a tela	
+}
+
+int cartorio()
 {
 	int opcao=0; //Definindo as variáveis
 	int laco=1; //Definindo a repetição
-	
+
 	for(laco=1;laco=1;)
 	{
 		
@@ -151,8 +235,9 @@ int main()
 	
 		setlocale(LC_ALL, "Portuguese"); //Definição do idioma
 	
-		printf("### Cartório da EBAC ###\n\n"); //Início do menu
-		printf("Escolha uma das opções do menu:\n\n"); 
+		printf("****** Cartório da EBAC ******\n\n"); //Mensagem para o usuário
+		printf("-------------xxxx-------------\n\n");
+		printf("Escolha uma das opções do menu:\n\n"); //Início do menu
 		printf("\t1 - Cadastrar usuários.\n");
 		printf("\t2 - Consultar ususários.\n");
 		printf("\t3 - Deletar usuários.\n"); 
@@ -162,30 +247,72 @@ int main()
 		scanf("%d", &opcao); //Armazenando a escolha do usuário na variável opção. É %d, pois é uma variável do tipo inteira
 	
 		system("cls"); //Esse comando limpa a tela, vai apagar todas as mensagens que tinha antes
-	
+
 		switch(opcao) //Início da seleção
 		{
 			case 1:
-				cadastrar(); //Chamando a função cadastrar
+				cadastrar_usuario(); //Chamando a função cadastrar
 				break; //Quando você terminar de colocar todas as instruções em um cenário no switch, você precisa colocar “break”, o break seria um ponto final. Se você não colocar não dará certo.
-				
 			case 2:
-				consultar(); //Chamando a função consultar
+				consultar_usuario(); //Chamando a função consultar
 				break; //Quando você terminar de colocar todas as instruções em um cenário no switch, você precisa colocar “break”, o break seria um ponto final. Se você não colocar não dará certo.
-				
 			case 3:
-				deletar(); //Chamando a função deletar
+				deletar_usuario(); //Chamando a função deletar
 				break; //Quando você terminar de colocar todas as instruções em um cenário no switch, você precisa colocar “break”, o break seria um ponto final. Se você não colocar não dará certo.
-			
 			case 4:
-				printf("Obrigado por ter utilizado o nosso sistema.\n");
-				return 0; //Quebrando a repetição do repetidor “for”, e assim o programa é fechado, também não foi necessário colocar o system(“pause”);
+				printf("Obrigado por ter utilizado o nosso sistema.\n"); //Mensagem para o usuário
+				exit (0); //Esse comando além de quebrar o loop do “for” ele também fehca o programa, ou seja, ele não volta pra função main. Os comandos return 0; e break; não fechariam o programa, mas sim a função e em seguida retornaria para a função main. E também não foi necessário colocar o system(“pause”); 
 				break; //Quando você terminar de colocar todas as instruções em um cenário no switch, você precisa colocar “break”, o break seria um ponto final. Se você não colocar não dará certo.
-				
 			default: //Esse default do switch VOCÊ PODE USAR quando a opção que o usuário escolheu não existe, aí você coloca o default em seguida coloca as instruções embaixo, NÃO PRECISA SER SÓ PRA OPÇÃO QUE NÃO EXISTE. Ele não é obrigatório
 				printf("Essa opção não existe.\n\n");
 				system("pause");
 				break; //Final da seleção
+		}
+	}	
+}
+
+int main()
+{
+	int login=0; //Definindo a variável da escolha do usuário
+	int rep=1; //Definindo a variável do repetidor “for”
+	
+	for(rep=1;rep=1;) //Comando do loop, para que ele fique repetindo incontáveis vezes
+	{
+		system("cls"); //Limpando a tela
+		
+		setlocale(LC_ALL, "portuguese"); //Definindo o idioma
+		
+		printf("**** Seja bem-vindo a área do administrador ****\n\n"); //Mensagem para o usuário
+		printf("Escolha uma das opções abaixo:\n\n"); //Mensagem para o usuário
+		printf("\t1 - Criar uma conta de administrador.\n"); //Início do menu
+		printf("\t2 - Entrar na cartório EBAC.\n");
+		printf("\t3 - Deletar sua conta de administrador.\n");
+		printf("\t4 - Sair do sistema.\n\n"); //Final do menu
+		printf("Opção: "); //Mensagem para o usuário
+		
+		scanf("%d", &login); //Escaneando e alocando a escolha do usuário na variável “login”
+		
+		system("cls"); //Limpando a tela
+		
+		switch(login) //Início da seleção
+		{
+			case 1:
+				criar_conta(); //Chamando a função “criar_conta”
+				break; //O ponto final do switch, se não colocar vai aparecer as instruções dos outros case
+			case 2: 
+				entrar_cartorio(); //Chamando a função “entrar_cartorio”
+				break; //O ponto final do switch, se não colocar vai aparecer as instruções dos outros case	
+			case 3:
+				deletar_conta(); //Chamando a função “deletar_conta”
+				break; //O ponto final do switch, se não colocar vai aparecer as instruções dos outros case
+			case 4: 
+				printf("Obrigado por ter utilizado o nosso sistema.\n"); //Mensagem para o usuário
+				return 0; //Quebrando a repetição do repetidor “for”, e assim o programa é fechado, também não foi necessário colocar o system(“pause”);
+				break; //O ponto final do switch, se não colocar vai aparecer as instruções dos outros case
+			default: //Quando o usuário escolher uma opção que não existe
+				printf("Essa opção não existe.\n\n"); //Mensagem para o usuário
+				system("pause"); //Pausando a tela
+				break; //O ponto final do switch, se não colocar vai aparecer as instruções dos outros case
 		}
 	}
 }
